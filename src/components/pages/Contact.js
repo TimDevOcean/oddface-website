50569d07566e70a0bc1d338dae9fceaa731551f5
Timothy Agozie <pc@TIM-MacBook.local>
2022-07-14 07:59:10 +0000

    Mobile nav minor styles..

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -6,7 +6,7 @@ import { Widget } from '@typeform/embed-react';
 
 export const Contact = () => {
   return (
-    <div className='app-container'>
+    <div className='app-container contact'>
         <main>
             <p>✉︎ info@oddfaceforever.com | ✆ +233 554 834 613</p><br/>
             <Widget id="EH6NuWRX" style={{ height:500 }} className="contact-form" />
